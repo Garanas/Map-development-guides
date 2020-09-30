82f71bbf7019524f207bfb0fdfb46678ec793b2b
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2020-09-30 11:57:56 +0200

    Adding water setttings files

---
+++ water - waves and normal maps/settings-b.lua
@@ -0,0 +1,39 @@
+Water = {
+surface_elev = 25.3,
+deep_elev = 0,
+abyss_elev = 0,
+surface_color = { 0.14, 0.9, 0.66 },
+color_lerp = { 0, 0.2 },
+refraction_scale = 0.1375,
+fresnel_bias = 0.1,
+fresnel_power = 0.9,
+unit_reflection = 0.73,
+sky_reflection = 0.844,
+sun_shininess = 53.2,
+sun_strength = 9.4,
+sun_direction = { 0.0995482, -0.962631, 0.251857 },
+sun_color = { 0.35, 0.19, 0.09 },
+sun_reflection = 3.12,
+sun_glow = 0.1,
+local_cubemap = '/textures/environment/SkyCube_TropicalOp06a.dds',
+water_ramp = '/textures/engine/waterramp.dds',
+normal_repeat = { 0.0045, 0.02, 0.2, 0.5 },
+normals = {
+['0'] = {
+normal_movement = { -0.269394, -0.0475015 },
+local_normals = '/textures/engine/waves000.dds',
+},
+['1'] = {
+normal_movement = { -0.0538961, 0.0196166 },
+local_normals = '/textures/engine/waves.dds',
+},
+['2'] = {
+normal_movement = { -0.046228, -2.02069e-009 },
+local_normals = '/textures/engine/waves001.dds',
+},
+['3'] = {
+normal_movement = { -0.00035213, 0.00096747 },
+local_normals = '/textures/engine/waves000.dds',
+},
+},
+}
---
+++ water - waves and normal maps/settings-default.lua
@@ -0,0 +1,39 @@
+Water = {
+surface_elev = 25.3,
+deep_elev = 0,
+abyss_elev = 0,
+surface_color = { 0.14, 0.9, 0.66 },
+color_lerp = { 0, 0.2 },
+refraction_scale = 0.1375,
+fresnel_bias = 0.1,
+fresnel_power = 0.9,
+unit_reflection = 0.73,
+sky_reflection = 0.844,
+sun_shininess = 53.2,
+sun_strength = 9.4,
+sun_direction = { 0.0995482, -0.962631, 0.251857 },
+sun_color = { 0.35, 0.19, 0.09 },
+sun_reflection = 3.12,
+sun_glow = 0.1,
+local_cubemap = '/textures/environment/SkyCube_TropicalOp06a.dds',
+water_ramp = '/textures/engine/waterramp.dds',
+normal_repeat = { 0.0009, 0.009, 0.05, 0.5 },
+normals = {
+['0'] = {
+normal_movement = { 0.504001, -0.947888 },
+local_normals = '/textures/engine/waves.dds',
+},
+['1'] = {
+normal_movement = { 0.0504001, -0.0947888 },
+local_normals = '/textures/engine/waves.dds',
+},
+['2'] = {
+normal_movement = { 0.0977198, 0.300751 },
+local_normals = '/textures/engine/waves.dds',
+},
+['3'] = {
+normal_movement = { 0.000499141, 0.000900474 },
+local_normals = '/textures/engine/waves.dds',
+},
+},
+}
